d0108643d309bb20d88a8bdb328723efec98a25c
armanwebsupport <armanraza232@gmail.com>
2026-07-12 05:30:47 +0530

    Implement counter animation on scroll
    Add counter animation functionality using Intersection Observer.

--- script.js
+++ script.js
@@ -144,3 +144,53 @@ function showTestimonial(){
 showTestimonial();
 
 setInterval(showTestimonial,4000);
+/* COUNTER ANIMATION */
+
+const counters = document.querySelectorAll(".counter");
+
+const counterObserver = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            const counter = entry.target;
+            const target = +counter.getAttribute("data-target");
+
+            let count = 0;
+
+            const updateCounter = () => {
+
+                const increment = target / 100;
+
+                if (count < target) {
+
+                    count += increment;
+
+                    counter.innerText = Math.ceil(count) + "+";
+
+                    requestAnimationFrame(updateCounter);
+
+                } else {
+
+                    counter.innerText = target + "+";
+
+                }
+
+            };
+
+            updateCounter();
+
+            counterObserver.unobserve(counter);
+
+        }
+
+    });
+
+});
+
+counters.forEach(counter => {
+
+    counterObserver.observe(counter);
+
+});
