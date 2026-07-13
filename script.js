1ada58b5e0ed043436ad819a140a3ea48d5a5939
armanwebsupport <armanraza232@gmail.com>
2026-07-13 21:42:23 +0530

    Implement FAQ item toggle on click
    Add FAQ toggle functionality to script.js

--- script.js
+++ script.js
@@ -257,3 +257,14 @@ window.addEventListener("scroll", () => {
         navbar.classList.remove("scrolled");
     }
 });
+// FAQ Toggle
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+        item.classList.toggle("active");
+    });
+});
